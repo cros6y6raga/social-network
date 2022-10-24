3a118287a35faa6b2261d563402646fa849682ca
cros6y6raga <crosbybraga@gmail.com>
2022-10-24 18:58:18 +0300

    10. React JS Lessons (default danger auto import plugin)

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import {Technologies} from "./Technologies";
 import {Header} from "./Header";
+import {Footer} from "./Footer";
 
 const App = () => {
     return (
         <div>
             <Header/>
             <Technologies/>
+            <Footer/>
         </div>
     );
 }
