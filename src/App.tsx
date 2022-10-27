36cfaff5929c0887594d49e71fe6291a8c7fa88c
cros6y6raga <crosbybraga@gmail.com>
2022-10-27 13:56:53 +0300

    12. React JS Lessons (Breakdown into Components)

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,15 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./components/Header";
+import {Navbar} from "./components/Navbar";
+import {Profile} from "./components/Profile";
 
 const App = () => {
     return (
         <div className='app-wrapper'>
-            <header className={'header'}>
-                <img src='https://www.tailorbrands.com/wp-content/uploads/2020/07/mcdonalds-logo.jpg'/>
-            </header>
-            <nav className='nav'>
-                <div>
-                    <a href='#'>Profile</a>
-                </div>
-                <div>
-                    <a href='#'>Messages</a>
-                </div>
-                <div>
-                    <a href='#'>News</a>
-                </div>
-                <div>
-                    <a href='#'>Music</a>
-                </div>
-                <div>
-                    <a href='#'>Settings</a>
-                </div>
-            </nav>
-            <div className='content'>
-                <div><img src='https://www.w3schools.com/w3css/img_lights.jpg'/></div>
-                <div>
-                    ava+descr
-                </div>
-                <div>
-                    My posts
-                    <div>
-                        New post
-                    </div>
-                    <div>
-                        <div>
-                            post1
-                        </div>
-                        <div>
-                            post2
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export  const Profile = () => {
+    return (
+        <div className='content'>
+            <div><img src='https://www.w3schools.com/w3css/img_lights.jpg'/></div>
+            <div>
+                ava+descr
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
