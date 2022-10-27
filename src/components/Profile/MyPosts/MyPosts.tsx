5312d82d458061ec4da2077792c95cec7249afc6
cros6y6raga <crosbybraga@gmail.com>
2022-10-27 16:47:06 +0300

    16. React JS Lessons (props are the most important topic)

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -12,11 +12,8 @@ export const MyPosts = () => {
                 <button>Remove</button>
             </div>
             <div className={s.posts}>
-               <Post/>
-               <Post/>
-               <Post/>
-               <Post/>
-               <Post/>
+                <Post message='Hi, how are you?'/>
+                <Post message="It's my first post"/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import s from './Post.module.css'
-
-export const Post = () => {
+type PostType={
+    message:string,
+}
+export const Post = (props:PostType) => {
     return (
         <div className={s.item}>
             <img src='https://lwlies.com/wp-content/uploads/2017/04/avatar-2009.jpg'/>
-            post1
+            {props.message}
             <div><span>like</span></div>
         </div>
     );
