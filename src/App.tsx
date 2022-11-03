e23a6c476cd7fb50970cd1b941c6a38dc5191d9f
cros6y6raga <crosbybraga@gmail.com>
2022-11-03 11:27:00 +0300

    19. React JS Lessons (route, browser-router, routing)

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,23 @@ import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 const App = () => {
     return (
+        <BrowserRouter>
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
             <div className={'app-wrapper-content'}>
-                <Dialogs/>
+                <Route path='/dialogs' component={Dialogs}/>
+                <Route path='/profile' component={Profile}/>
+                <Route path='/news'/>
+                <Route path='/music'/>
+                <Route path='/settings'/>
             </div>
-            {/*<Profile/>*/}
         </div>
+        </BrowserRouter>
     );
 }
 
