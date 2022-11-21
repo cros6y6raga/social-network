af791ce186ede146f80f424cfd4d539151435437
cros6y6raga <crosbybraga@gmail.com>
2022-11-21 13:08:20 +0300

    24. React JS Lessons (UI - BLL, taking data to the side)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,9 +3,10 @@ import s from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
 type DialogStringType = {
-    name: string,
-    id: string
+    name:string
+    id: number
 }
+
 type MessageType = {
     message: string
 }
@@ -19,17 +20,27 @@ const Message = (props: MessageType) => {
     return <div className={s.message}>{props.message}</div>
 }
 export const Dialogs = () => {
+    let dialogsData = [
+        {id: 1, name: 'React'},
+        {id: 2, name: 'Angular'},
+        {id: 3, name: 'Vue'},
+    ]
+    let messagesData = [
+        {id: 1, message: 'My name is react'},
+        {id: 2, message: 'My name is angular'},
+        {id: 3, message: 'My name is vue'},
+    ]
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name='React' id='1'/>
-                <DialogItem name='Angular' id='2'/>
-                <DialogItem name='Vue' id='3'/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
             </div>
             <div className={s.messages}>
-                <Message message='My name is react'/>
-                <Message message='My name is angular'/>
-                <Message message='My name is vue'/>
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
+                <Message message={messagesData[2].message}/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,10 @@ import s from './MyPosts.module.css'
 import {Post} from "./Post/Post";
 
 export const MyPosts = () => {
+    let postData = [
+        {id: 1, message: 'Hello', likesCount: '23'},
+        {id: 2, message: 'Angular is the coolest', likesCount: '40'},
+    ]
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
@@ -15,8 +19,8 @@ export const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message='Hello' likesCount='23'/>
-                <Post message='Angular is the coolest' likesCount='40'/>
+                <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
+                <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
             </div>
         </div>
     );
