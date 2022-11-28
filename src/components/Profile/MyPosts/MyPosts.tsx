b8322865963aef069b8a1f051fefd7fae71f4e6a
cros6y6raga <crosbybraga@gmail.com>
2022-11-28 11:21:06 +0300

    Refactoring

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import s from './MyPosts.module.css'
 import {Post} from "./Post/Post";
+import {PostType} from "../Profile";
 
-export const MyPosts = () => {
-    let posts = [
-        {id: 1, message: 'Hello', likesCount: '23'},
-        {id: 2, message: 'Angular is the coolest', likesCount: '40'},
-    ]
-    let postsElements = posts.map(p=>  <Post message={p.message} likesCount={p.likesCount}/>)
+
+
+type PropsType = {
+    posts: PostType[]
+}
+
+
+export const MyPosts = (props:PropsType) => {
+
+    let postsElements = props.posts.map(p=>  <Post message={p.message} likesCount={p.likesCount}/>)
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,11 +3,24 @@ import s from './Profile.module.css'
 import {MyPosts} from "./MyPosts/MyPosts";
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 
+
+export type PostType = {
+    id: number
+    message:string
+    likesCount: string
+}
+
+
 export const Profile = () => {
+
+    let posts: PostType[] = [
+        {id: 1, message: 'Hello', likesCount: '23'},
+        {id: 2, message: 'Angular is the coolest', likesCount: '40'},
+    ]
     return (
         <div>
             <ProfileInfo/>
-            <MyPosts/>
+            <MyPosts posts={posts}/>
         </div>
     );
 };
