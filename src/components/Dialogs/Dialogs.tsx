0a704a726f1f1a124ec3c6ed6c331fea40af25db
cros6y6raga <crosbybraga@gmail.com>
2022-11-29 14:02:39 +0300

    Part 29 of lesson

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -5,8 +5,7 @@ import {DialogItem} from "./DialogItem/DialogItem";
 import {DialogsType, MessagesPageType, MessagesType, ProfilePageType, RootStateType} from "../../redux/state";
 
 type DialogStringType = {
-    dialogs: MessagesPageType
-    messages: MessagesType[]
+    state: MessagesPageType
 }
 // type DialogPageType={
 //     dialogs:DialogsType[]
@@ -15,8 +14,8 @@ type DialogStringType = {
 
 export const Dialogs = (props: DialogStringType) => {
 
-    let dialogsElement = props.dialogs.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
-    let messagesElement = props.messages.map(m => <Message message={m.message} id={m.id}/>)
+    let dialogsElement = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
+    let messagesElement = props.state.messages.map(m => <Message message={m.message} id={m.id}/>)
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
