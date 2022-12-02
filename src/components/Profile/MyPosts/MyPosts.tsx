615a122f66005821bcad8069c7d7d9601178a453
cros6y6raga <crosbybraga@gmail.com>
2022-12-02 13:33:42 +0300

    31. React JS Lessons - onClick, ref, VirtualDOM

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {LegacyRef, useRef} from 'react';
 import s from './MyPosts.module.css'
 import {Post} from "./Post/Post";
 import {PostType} from "../../../redux/state";
@@ -9,15 +9,21 @@ type PropsType = {
 
 export const MyPosts = (props: PropsType) => {
     let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
+    let newPostEl = useRef<HTMLTextAreaElement>(null)
+    const addPost = () => {
+        if (newPostEl.current !== null) {
+            alert(newPostEl.current.value)
+        }
+    }
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostEl}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
