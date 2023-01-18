44bd52c1edf07d86a1a174ce11723d4d49b74c02
cros6y6raga <crosbybraga@gmail.com>
2023-01-18 14:48:39 +0300

    start 42+2

--- src/redux/redux-store.tsx
+++ src/redux/redux-store.tsx
@@ -1,5 +1,10 @@
 import {combineReducers, createStore} from "redux";
+import {ProfileReducer} from "./profile-reducer";
+import {DialogsReducer} from "./dialogs-reducer";
 
-const reducers = combineReducers({})
+const reducers = combineReducers({
+    profilePage:ProfileReducer,
+    dialogsPage:DialogsReducer
+})
 
-let store = createStore(reducers)
+export let store = createStore(reducers)
