c9e10f998f1aa628f3d9ea7312b4273085994163
cros6y6raga <crosbybraga@gmail.com>
2023-02-01 09:08:33 +0300

    44

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,18 @@ import './index.css';
 import App from "./App";
 import {BrowserRouter} from "react-router-dom";
 import {store} from "./redux/redux-store";
+import StoreContext from "./StoreContext";
 
 export let rerenderEntireTree = (state:any) => {
 
     ReactDOM.render(
         <BrowserRouter>
+            <StoreContext.Provider value={store}>
             <App state={store.getState()}
                  dispatch={store.dispatch.bind(store)}
                  store={store}
             />
+            </StoreContext.Provider>
         </BrowserRouter>,
         document.getElementById('root'));
 }
