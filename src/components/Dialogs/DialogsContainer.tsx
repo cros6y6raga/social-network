0102ed03939ad91067f83f3d2f70078f10534aa7
cros6y6raga <crosbybraga@gmail.com>
2023-02-16 10:38:12 +0300

    45 incomplete

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
 import {sendMessageAC, updateNewMessageAC} from "../../redux/dialogs-reducer";
 import {Dialogs} from "./Dialogs";
-import StoreContext from "../../StoreContext";
+import {connect} from "react-redux";
 
-export const DialogsContainer = () => {
-    return <StoreContext.Consumer>
-        {(store) => {
-            let state = store.getState().dialogsPage
-            let onSendMessageClick = () => {
-                store.dispatch(sendMessageAC())
-            }
-            let onNewMessageChange = (body: any) => {
-                store.dispatch(updateNewMessageAC(body))
-            }
-            return <Dialogs
-                updateNewMessage={onNewMessageChange}
-                sendMessage={onSendMessageClick}
-                dialogsPage={state}
-            />
+// export const DialogsContainer = () => {
+//     return <StoreContext.Consumer>
+//         {(store) => {
+//             let state = store.getState().dialogsPage
+//             let onSendMessageClick = () => {
+//                 store.dispatch(sendMessageAC())
+//             }
+//             let onNewMessageChange = (body: any) => {
+//                 store.dispatch(updateNewMessageAC(body))
+//             }
+//             return <Dialogs
+//                 updateNewMessage={onNewMessageChange}
+//                 sendMessage={onSendMessageClick}
+//                 dialogsPage={state}
+//             />
+//         }
+//         }
+//     </StoreContext.Consumer>
+// };
+
+let mapStateToProps = (state: any) => {
+    return {
+        dialogsPage: state.dialogsPage
+    }
+}
+
+let mapDispatchToProps = (dispatch: any) => {
+    return {
+        updateNewMessage: () => {
+            dispatch(sendMessageAC())
+        },
+        sendMessage: (body: any) => {
+            dispatch(updateNewMessageAC(body))
         }
-        }
-    </StoreContext.Consumer>
-};
+    }
+}
+
+export const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs);
--- src/components/Profile/MyPosts/MyPostsContainer.tsx
+++ src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {addPostAC, changeNewTextAC} from "../../../redux/profile-reducer";
 import {MyPosts} from "./MyPosts";
 import StoreContext from "../../../StoreContext";
+import {connect} from "react-redux";
+import {store} from "../../../redux/store";
 
 export const MyPostsContainer = () => {
     return (
@@ -26,3 +28,24 @@ export const MyPostsContainer = () => {
         </StoreContext.Consumer>
     );
 };
+
+const mapStateToProps = (state: any) => {
+    return {
+        posts: state.profilePage.posts,
+        newPostText: state.profilePage.newPostText
+    }
+}
+const mapDispatchToProps = (dispatch: any) => {
+    return {
+        updateNewPostText: (text: any) => {
+            let action = changeNewTextAC(text)
+            dispatch(action)
+        },
+        addPost: () => {
+            dispatch(addPostAC(store._state.profilePage.newPostText))
+        }
+    }
+}
+
+
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
