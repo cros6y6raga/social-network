648fc17f15d394073056276da251ee3af0df6957
cros6y6raga <crosbybraga@gmail.com>
2023-03-01 12:47:42 +0300

    54. Life cycle, life cycle methods, componentDidMount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,13 +5,12 @@ import axios from "axios";
 import userPhoto from '../../assets/images/user.png'
 
 export class Users extends React.Component<UsersPropsType> {
-    constructor(props:UsersPropsType) {
-        super(props);
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                this.props.setUsers(response.data.items)
-            })
-        // <button onClick={this.getUsers}>Get Users</button>
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUsers(response.data.items)
+        })
     }
+
     render() {
         return (
             <div>
